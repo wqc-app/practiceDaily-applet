33bc042f857227c5639b3cfb8d57672451e31016
selfridicule <selfridicule@gmail.com>
2023-07-27 15:15:55 +0800

    描述：底部footer

---
+++ components/self-footer/self-footer.js
@@ -0,0 +1,25 @@
+// components/self-footer/self-footer.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        time :  new Date().getFullYear()
+    },
+
+
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        
+    }
+})
