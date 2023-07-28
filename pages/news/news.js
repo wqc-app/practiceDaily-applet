a100637d2d2ee4e9d333df2333b3005a4fec81c1
selfridicule <selfridicule@gmail.com>
2023-07-28 17:02:15 +0800

    描述：下拉刷新数据，上拉加载数据

--- pages/news/news.js
+++ pages/news/news.js
@@ -13,6 +13,10 @@ Page({
      * 页面的初始数据
      */
     data: {
+        "loading" :false,
+        "noData" : false,
+        "limit": 8, //获取多少个
+        "size": 0, //分页（过滤的个数）
         newList: []
     },
 
@@ -25,20 +29,37 @@ Page({
 
     // 获取最新动态数据
     getNewsData() {
+        if(this.data.noData){
+            return 
+        }
+        this.setData({
+            "loading" : true
+        })
         let data = {
-            "limit": 8, //获取多少个
-            "size": 0, //分页（过滤的个数）
+            "limit": this.data.limit,
+            "size": this.data.size,
         }
         getNewsDataRequest(data).then(res => {
             console.log('getNewsData', res);
             let list = res.data.data;
+            
             list.forEach(item => {
                 item.publish_date = formattedDate(item.publish_date)
                 item.view_count = formatNumber(item.view_count)
             })
+            this.data.newList = this.data.newList.concat(list)
             this.setData({
-                newList: list
+                newList: this.data.newList,
+                "loading" : false
             })
+            // 判断是否没有更多数据
+            if(list.length <= 0){
+                this.setData({
+                    "noData" : true
+                })
+            }
+            // 关闭下拉刷新
+            wx.stopPullDownRefresh()
         })
     },
 
@@ -74,14 +95,23 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh() {
-
+        console.log('下拉刷新!');
+        this.setData({
+            "size" : 0 ,
+            "loading" :false,
+            "noData" : false,
+            "newList" : []
+        })
+        this.getNewsData();
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom() {
-
+        console.log('拉到底部！');
+        this.data.size = this.data.size + this.data.limit, 
+        this.getNewsData();
     },
 
     /**
