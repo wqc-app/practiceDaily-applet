cdbefe3266b8ed221148394453981216b7098b0d
selfridicule <selfridicule@gmail.com>
2023-07-28 11:43:24 +0800

    描述：资讯

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,15 +18,7 @@ Page({
             classname: "ikun",
             icon: "/static/imgs/ikun.jpg",
         }],
-        newList: [{
-            _id: "1",
-            title: "大家好，我是练习两年半的ikun，擅长唱、跳、rap!1111111111111",
-            picurl: "/static/imgs/ikun.jpg",
-            publish_date: 1672486225000,
-            view_count: 999,
-            author: "ikun",
-            is_essence: true,
-        }]
+        newList: []
     },
 
     /**
--- pages/news/news.js
+++ pages/news/news.js
@@ -1,3 +1,11 @@
+import {
+    formatNumber,
+    formattedDate
+} from '../../utils/common'
+import {
+    getNewsDataRequest
+} from '../../api/all'
+
 // pages/news/news.js
 Page({
 
@@ -5,14 +13,33 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        newList: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
+        this.getNewsData();
+    },
 
+    // 获取最新动态数据
+    getNewsData() {
+        let data = {
+            "limit": 8, //获取多少个
+            "size": 0, //分页（过滤的个数）
+        }
+        getNewsDataRequest(data).then(res => {
+            console.log('getNewsData', res);
+            let list = res.data.data;
+            list.forEach(item => {
+                item.publish_date = formattedDate(item.publish_date)
+                item.view_count = formatNumber(item.view_count)
+            })
+            this.setData({
+                newList: list
+            })
+        })
     },
 
     /**
